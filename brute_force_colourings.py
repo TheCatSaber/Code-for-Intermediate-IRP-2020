3b4b9f52a1d79a5919186c439899b4961f66852d
TheCatSaber <66741113+TheCatSaber@users.noreply.github.com>
2020-08-31 11:59:43 +0100

    Add License info at top of file

--- brute_force_colourings.py
+++ brute_force_colourings.py
@@ -6,6 +6,20 @@ In summary, it counts in a variable base system,
 but only tests if it is a valid colouring if it uses all the colours
 from 1 to the number of colours being tested to prevent repeats.
 """
+# Copyright (C) 2020 TheCatSaber
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #====Imports====#
 
